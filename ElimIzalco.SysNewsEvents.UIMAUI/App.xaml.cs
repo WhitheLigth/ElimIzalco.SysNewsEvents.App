6fb7d2903fa202dab09b6599706e28d9c4944e6d
Hector Garcia <hg425823@gmail.com>
2023-12-02 23:21:07 -0600

    Agregar archivos de proyecto.

---
+++ ElimIzalco.SysNewsEvents.UIMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ElimIzalco.SysNewsEvents.UIMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ElimIzalco.SysNewsEvents.UIMAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ElimIzalco.SysNewsEvents.UIMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
